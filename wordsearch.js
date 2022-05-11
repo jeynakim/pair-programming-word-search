ded99e44f775768cc628084b65c5aa7be28e8a09
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-11 22:11:15 +0000

    finished Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
+const transpose = function(matrix) {
+  
+    let answer = []
+    for (let i = 0; i < matrix[0].length; i++){
+      answer.push([]);
+    }
+  
+    for (let x = 0; x < matrix.length; x++) {
+      for (let y = 0; y < matrix[x].length; y++) {
+         answer[y][x] = matrix[x][y];
+      }
+    }
+        return answer;
+  };
+
+
+const wordSearch = (letters, word) => {
+
+    const vertical = transpose(letters);
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
-}
+    const verticalJoin = vertical.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
+    }
+    return false
+  }
+  
+  
+  module.exports = wordSearch
+ 
 
-module.exports = wordSearch
